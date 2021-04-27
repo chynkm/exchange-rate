f0131ed05f8ce5d81667371037ce100c460db6fb
KM <chynkm@gmail.com>
2021-04-27 21:54:32 +0530

    2040 - 2155 Created currency CSV file fetch and unzip (1.25)

---
+++ currencystore/rate.go
@@ -0,0 +1,50 @@
+package currencystore
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+const (
+	csvZipFile = "/tmp/eurofxref.zip"
+	csvFile    = "/tmp/eurofxref.csv"
+)
+
+// DownloadCsv the CSV file and save it to /tmp
+func DownloadCsv(url string) error {
+	deleteCurrencyFiles()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	out, err := os.Create(csvZipFile)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command("/usr/bin/unzip", csvZipFile)
+	cmd.Dir = "/tmp"
+	return cmd.Run()
+}
+
+// deleteCurrencyFiles: remove existing files before downloading
+func deleteCurrencyFiles() error {
+	err := os.Remove(csvZipFile)
+	if err != nil {
+		return err
+	}
+
+	err = os.Remove(csvFile)
+	return err
+}

dc4d03c639217cb9671277108585e3d824c97c0c
KM <chynkm@gmail.com>
2021-05-01 14:30:00 +0530

    1344 - 1430 Completed Exchange rate hash creation for different base currencies (0.77)

--- redisdb/exchange_rate.go
+++ redisdb/exchange_rate.go
@@ -4,13 +4,42 @@ import (
 	"math"
 )
 
-func getExchangeRate(baseCurrencyCode string, exchangeRates map[string]float64) map[string]float64 {
-	hash := map[string]float64{}
+const euro = "EUR"
+
+// createExchangeRateHash generates hash for exchange rates of different
+// currencies for the given date
+func createExchangeRateHash(
+	date string,
+	currencies map[string]int,
+	exchangeRates map[string]float64,
+) map[string]map[string]float64 {
+	exchangeRateHash := map[string]map[string]float64{}
+
+	for dbCurrencyCode, _ := range currencies {
+		exchangeRateHash[date+":"+dbCurrencyCode] = getExchangeRate(
+			dbCurrencyCode,
+			exchangeRates,
+		)
+	}
+
+	return exchangeRateHash
+}
+
+// getExchangeRate for a base currency
+func getExchangeRate(
+	baseCurrencyCode string,
+	exchangeRates map[string]float64,
+) map[string]float64 {
+	if baseCurrencyCode == euro {
+		return exchangeRates
+	}
+
+	baseCurrencyExchangeRate := map[string]float64{}
 	for currencyCode, rate := range exchangeRates {
 		roundRate := rate / exchangeRates[baseCurrencyCode]
 		roundRate = math.Round(roundRate*10000) / 10000
-		hash[currencyCode] = roundRate
+		baseCurrencyExchangeRate[currencyCode] = roundRate
 	}
 
-	return hash
+	return baseCurrencyExchangeRate
 }
--- redisdb/exchange_rate_test.go
+++ redisdb/exchange_rate_test.go
@@ -5,6 +5,42 @@ import (
 	"testing"
 )
 
+func TestExchangeRateFormat(t *testing.T) {
+	currencies := map[string]int{
+		"EUR": 1,
+		"USD": 2,
+		"JPY": 3,
+	}
+	exchangeRates := map[string]float64{
+		"EUR": 1,
+		"USD": 1.2082,
+		"JPY": 131.6200,
+	}
+
+	got := createExchangeRateHash("2021-04-25", currencies, exchangeRates)
+	want := map[string]map[string]float64{
+		"2021-04-25:EUR": {
+			"EUR": 1,
+			"USD": 1.2082,
+			"JPY": 131.62,
+		},
+		"2021-04-25:USD": {
+			"EUR": 0.8277,
+			"USD": 1,
+			"JPY": 108.9389,
+		},
+		"2021-04-25:JPY": {
+			"EUR": 0.0076,
+			"USD": 0.0092,
+			"JPY": 1,
+		},
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Error("exchange rate hash creation failed")
+	}
+}
+
 func TestGetExchangeRate(t *testing.T) {
 	exchangeRates := map[string]float64{
 		"EUR": 1,

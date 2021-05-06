b19b067684aa41f00a2b830a5e066575c8cf31d2
KM <chynkm@gmail.com>
2021-05-06 22:10:19 +0530

    2030 - 2054 Analyzed about API rate limiting validation functions[no coding] (0.40) 2054 - 2154 Coded key creation, fetching key count & incrementing key (1.00) 2154 - 2212 Tweaked key fetch with exists & tested AllowAPIRequest (0.30)

---
+++ redisdb/api_request.go
@@ -0,0 +1,77 @@
+package redisdb
+
+import (
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+const (
+	api_user_prefix      = "api_user:"
+	max_requests_per_min = 100
+	fixed_window         = 60 // start of minute to end of minute
+)
+
+// incrementKey increases the count by 1 if key is present.
+// if missing, creates a key and sets the value to 1 and also
+// adds an expiry counter to the key
+func incrementKey(rdb redis.Conn, key string, seconds int) {
+	_, err := rdb.Do("INCR", key)
+	if err != nil {
+		log.Fatal("redis: unable to set count for key: ", key)
+	}
+
+	if seconds > 0 {
+		_, err = rdb.Do("EXPIRE", key, seconds)
+		if err != nil {
+			log.Fatal("redis: unable to set expiry for key: ", key)
+		}
+	}
+}
+
+// getKeyCount retrieves the key count
+func getKeyCount(rdb redis.Conn, key string) int {
+	exists, err := redis.Int(rdb.Do("EXISTS", key))
+	if err != nil {
+		log.Fatal("redis: check key exists failed. key: ", key)
+	}
+
+	if exists == 0 {
+		return 0
+	}
+
+	count, err := redis.Int(rdb.Do("GET", key))
+	if err != nil {
+		log.Fatal("redis: unable to get count for key: ", key)
+	}
+
+	return count
+}
+
+// getKeyCount creates the key with prefix and suffix
+func createKey(ip string, t time.Time) string {
+	return api_user_prefix + ip + ":" + strconv.Itoa(t.Minute())
+}
+
+// AllowAPIRequest determines whether an API request should be allowed or not
+func AllowAPIRequest(ip string) bool {
+	rdb := Rdbpool.Get()
+	defer rdb.Close()
+
+	t := time.Now()
+	key := createKey(ip, t)
+
+	n := getKeyCount(rdb, key)
+	if n >= max_requests_per_min {
+		return false
+	}
+
+	seconds := 0
+	if n == 0 {
+		seconds = fixed_window - t.Second()
+	}
+	incrementKey(rdb, key, seconds)
+	return true
+}
--- redisdb/api_request_test.go
+++ redisdb/api_request_test.go
@@ -1,1 +1,18 @@
 package redisdb
+
+import (
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestCreateKey(t *testing.T) {
+	time := time.Now()
+	ip := "::1"
+	got := createKey(ip, time)
+	want := api_user_prefix + ip + strconv.Itoa(time.Minute())
+
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
